7648dc0a135fc49da13c4e90e4dcfd66ee22862e
Roman <romantisenko3@gmail.com>
2023-06-26 03:21:59 +0400

    add block corousel

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.corousel__wrapper').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        speed: 3000,
+        arrows: false
+    });
+});
